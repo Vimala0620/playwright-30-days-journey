62e9434845ce656eea41650d4ced0477b1c2ea31
vimala <vimala.v@cloudfulcrum.com>
2026-02-28 07:58:58 +0530

    Enable only the first test and log titles

--- day2/UIBasicstest.spec.js
+++ day2/UIBasicstest.spec.js
@@ -1,17 +1,23 @@
 const { test, expect } = require('@playwright/test');
 
-test('Browser context playwright test', async ({ browser }) => {
+test.only('Browser context playwright test', async ({ browser }) => {
 
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto('https://rahulshettyacademy.com/loginPagePractise/');
-
+  const title = await page.title();
+  console.log(title);
   await expect(page).toHaveTitle(/Login/);
 
+  await page.locator('#username').fill('rahulshettyacademy');
+  await page.locator('#password').fill('learning');
+  await page.locator('#signInBtn').click();
+  await expect(page.locator('.text-center')).toHaveText('Welcome, rahulshettyacademy');
 });
 
 test('Pageplaywright test', async ({ page }) => {
   await page.goto('https://www.google.com/');
-
+  const title = await page.title();
+  console.log(title);
   await expect(page).toHaveTitle(/Google/);
 });
