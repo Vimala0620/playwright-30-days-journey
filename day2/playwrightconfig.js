c32da05ef89e9fd49e718dc0f908f615f863220c
vimala <vimala.v@cloudfulcrum.com>
2026-02-28 06:30:45 +0530

    Add Playwright configuration file

---
+++ day2/playwrightconfig.js
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+const config = ({
+  testDir: './tests',
+  reporter: 'html',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },  
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    browserName: 'chromium',
+  },
+    
+  });
+  module.exports = config;
+
